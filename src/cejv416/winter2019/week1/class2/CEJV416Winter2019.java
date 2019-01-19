347fb1d0aad3ffd23746a62524a0e106a0bb0b1a
j_zho11 <j_zho11@CCE-FB107-21W.concordia.ca>
2019-01-18 19:41:00 -0500

    initial commet

--- src/cejv416/winter2019/week1/class2/CEJV416Winter2019.java
+++ src/cejv416/winter2019/week1/class2/CEJV416Winter2019.java
@@ -1,3 +1,5 @@
+package cejv416.winter2019.week1.class2;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,6 +18,7 @@ public class CEJV416Winter2019 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ohhhhhh");
     }
     
 }
