55026b1435ab8af38dd3444f5990b92546e8ef6a
j_zho11 <j_zho11@CCE-FB107-21W.concordia.ca>
2019-01-18 21:00:02 -0500

    third commet

---
+++ src/cejv416/winter2019/week1/class2/Age.java
@@ -0,0 +1,38 @@
+package cejv416.winter2019.week1.class2;
+import java.util.Scanner;
+
+
+public class Age {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Age Calculator");
+        System.out.println();  // print a blank line
+
+        // create a Scanner object named sc
+        Scanner sc = new Scanner(System.in);
+
+        // perform invoice calculations until choice isn't equal to "y" or "Y"
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y"))
+        {
+            // get the invoice subtotal from the user
+            System.out.print("Enter The year you born:   ");
+            double year = sc.nextDouble();
+
+    
+
+            double age = 2019 - year;
+
+            String message = "Your age is: " + age + "\n"
+                     + "The number of month you live is:   " + age*12 + "\n"
+                    + "The number of day you live is:   " + age*12*365 + "\n";
+            System.out.println(message);
+
+            // see if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+         }
+    }
+}
---
+++ src/cejv416/winter2019/week1/class2/NewMain.java
@@ -0,0 +1,28 @@
+package cejv416.winter2019.week1.class2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author j_zho11
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int year = 2019;
+        double mysalary = 10.00;
+        char myfavoritenumber = 400;
+        
+        
+    }
+    
+}
