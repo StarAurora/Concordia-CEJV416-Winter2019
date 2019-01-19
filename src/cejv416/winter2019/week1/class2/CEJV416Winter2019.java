b2089ff62ab0adf1084320c701e955df6270784c
j_zho11 <j_zho11@CCE-FB107-21W.concordia.ca>
2019-01-18 19:43:43 -0500

    second commet, add some output

--- src/cejv416/winter2019/week1/class2/CEJV416Winter2019.java
+++ src/cejv416/winter2019/week1/class2/CEJV416Winter2019.java
@@ -18,7 +18,7 @@ public class CEJV416Winter2019 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ohhhhhh");
+        System.out.println("Ohhhhhh eh hei hei");
     }
     
 }
